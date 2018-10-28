2accdfc2a8dd7d8b8ceef79cbbaf1f569817c4c3
rejbo.dawid@gmail.com <rejbo.dawid@gmail.com>
2018-10-28 11:11:26 +0100

    first commit

---
+++ src/main/java/zadanie28/zadanie28/Zadanie28Application.java
@@ -0,0 +1,12 @@
+package zadanie28.zadanie28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie28Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zadanie28Application.class, args);
+	}
+}
---
+++ src/test/java/zadanie28/zadanie28/Zadanie28ApplicationTests.java
@@ -0,0 +1,16 @@
+package zadanie28.zadanie28;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Zadanie28ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
