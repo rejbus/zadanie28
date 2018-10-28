d993aabbc0eb7467dc8711110d8800b92819f097
rejbo.dawid@gmail.com <rejbo.dawid@gmail.com>
2018-10-28 11:42:28 +0100

    add a config

---
+++ src/main/java/zadanie28/zadanie28/model/Post.java
@@ -0,0 +1,19 @@
+package zadanie28.zadanie28.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date added = new Date();
+
+}
